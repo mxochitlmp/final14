139213e41089046c7873bf2f355e424b76c42b49
mxochitlmp <mather02@hotmail.com>
2014-02-14 12:40:24 -0600

    bien

---
+++ js/acciones.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+// JavaScript Document
+$(document).ready(
+					function()
+					{							
+						 document.addEventListener("deviceready",
+						               function()
+						 			   {
+										 $('#b1').tap(
+										 			   function()
+													   {
+														  navigator.notification.beep(1);
+													    }
+										 			)
+									   }
+									  );								
+					}	
+				 );
+				 
+				 /*
+				 $('.clase')
+				 $('#nombre')
+				 $('p')
+				 */
+				 
+				 
